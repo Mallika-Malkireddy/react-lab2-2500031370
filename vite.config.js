2f2ceb23415e040fa1a0c7f85f64f19a2e0c474e
mallika <2500031370@kluniversity.in>
2026-03-12 22:34:29 +0530

    modofied project

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"react-lab2-2500031370"
+  base:"/react-lab2-2500031370/"
 })
